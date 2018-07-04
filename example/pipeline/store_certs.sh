29854e6f386e14b1bd4a0098ba6c8a5e46412f2d
Sami Alajrami <sami@praqma.com>
2018-07-04 19:23:49 +0200

    fixing tf output variable name and shell variable definition

--- example/pipeline/store_certs.sh
+++ example/pipeline/store_certs.sh
@@ -3,14 +3,15 @@
 
 # The bucket where the certs will be stored. It should be provided in an env variable called 'GCS_KEYS_BUCKET' 
 # The format is : gs://bucket-name
-bucket = ${GCS_KEYS_BUCKET}
+bucket=${GCS_KEYS_BUCKET}
 # saving the scecrets into files
 echo "Importing keys from terraform into local files for k8s cluster ... "
 
 mkdir keys
+# base64 -d doesn't work on Mac, use -D instead
 echo $(terraform output cluster_client_certificate) | base64 -d > keys/client_certificate.crt
 echo $(terraform output cluster_client_key) | base64 -d > keys/client_key.key
-echo $(terraform output cluster_cluster_ca_certificate) | base64 -d > keys/ca.crt
+echo $(terraform output cluster_ca_certificate) | base64 -d > keys/ca.crt
 
 # push secrets to GCS and cleanup the local file system
 if [[ -s keys/client_certificate.crt && -s keys/client_key.key && -s keys/ca.crt ]];then
